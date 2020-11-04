c3af2b1df5f14ef16e38142e10ee01a6b834de69
Kubaszolc01 <42960682+Kubaszolc01@users.noreply.github.com>
2020-11-04 12:25:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,9 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kuba Szolc</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <h1>Kuba Szolc gr.1 nr 13</h1>
+    <header class="header">
+        <a href="https://github.com/3ti-2020" class="githublink"> <img class="githubimg" src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="GITHUB"> </a>
+        <h1>Kuba Szolc gr.1 nr 13</h1>
+        <ul class="menu">
+            <li class="mlink">
+                <a href="crud/index.php">CRUD</a>
+            </li>
+            <li class="mlink">
+                <a href="card/index.html">KARTY</a>
+            </li>
+            <!-- <li class="mlink">
+                <a href="slider/index.html">SLIDER</a>
+            </li> -->
+        </ul>
+    </header>
     <div class="tablecont">
         <table>
         <?php
