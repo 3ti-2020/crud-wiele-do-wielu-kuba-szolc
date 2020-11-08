41475b3956da0e5739eef7049001aded7c6bc07e
Kubaszolc01 <42960682+Kubaszolc01@users.noreply.github.com>
2020-11-08 20:34:25 +0100

    Add files via upload

--- logowanie/rejestracja.php
+++ logowanie/rejestracja.php
@@ -2,17 +2,25 @@
     $user = $_POST['user'];
     $haslo = $_POST['haslo'];
 
-    // $conn = new mysqli("localhost", "root", "zaq1@WSX", "library");
-    $conn = new mysqli("remotemysql.com", "1Ed39FMiyQ", "ZMFu5eO2lq", "1Ed39FMiyQ");
+    if($user != '' && $haslo != ''){
+        // $conn = new mysqli("localhost", "root", "zaq1@WSX", "library");
+        $conn = new mysqli("remotemysql.com", "1Ed39FMiyQ", "ZMFu5eO2lq", "1Ed39FMiyQ");
 
-    $sql = "INSERT INTO users VALUES ('$user', '$haslo')";
+        $sql = "INSERT INTO users VALUES ('$user', '$haslo')";
 
-    mysqli_query($conn, $sql);
+        mysqli_query($conn, $sql);
 
-    session_start();
-    if(isset($_SESSION['fail'])){
-        unset($_SESSION['fail']);
+        session_start();
+        if(isset($_SESSION['fail'])){
+            unset($_SESSION['fail']);
+        }
+        if(isset($_SESSION['regFail'])){
+            unset($_SESSION['regFail']);
+        }
+    }else{
+        $_SESSION['regFail'] = 1;
     }
 
+    include("uwierzytelnianie.php");
     header('Location: index.php');
 ?>
